fccf06dcc51d512e49a5939b131d27936ac68285
mzaini30 <muhzaini30@gmail.com>
2021-06-30 15:43:14 +0800

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import {Route} from 'solid-app-router'
+import {Portal} from 'solid-js/web'
+import './App.css'
 
 const App = () => (
-  <Route></Route>
+  <div>
+    <Route></Route>
+
+    <Portal mount={document.head}>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+    </Portal>
+  </div>
 )
 
 export default App
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,10 @@ import {Portal} from 'solid-js/web'
 
 const Index = () => (
 	<div>
+		<div className="p-5">
+			<h1 className='text-3xl'>Bentar. Belum selesai blognya...</h1>
+		</div>
+
 		<Portal mount={document.head}>
 			<title>Blog</title>
 		</Portal>
