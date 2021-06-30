257a0fc2e42feb286f532de03454229021b6cabe
mzaini30 <muhzaini30@gmail.com>
2021-06-30 17:28:07 +0800

    upload

--- src/pages/admin/tulis.jsx
+++ src/pages/admin/tulis.jsx
@@ -1,7 +1,10 @@
 import {Portal} from 'solid-js/web'
-import {createSignal} from 'solid-js'
+import {createSignal, createEffect} from 'solid-js'
 
 const [data, setData] = createSignal({tulisan: '<p>hello <strong>world</strong></p>'})
+const [tulisan, setTulisan] = createSignal('')
+
+createEffect(() => setTulisan(data().tulisan))
 
 const Tulis = () => (
 	<div>
@@ -11,7 +14,13 @@ const Tulis = () => (
 			</div>
 			<div className="col-span-3">
 				<div className="rounded grid gap-2 grid-cols-2 bg-green-100 p-3 shadow bordered" style='height: calc(100vh - 70px)'>
-					<div className="focus:outline-none p-2 bg-green-200 overflow-auto rounded" contenteditable onInput={x => setData({tulisan: x.target.innerHTML})} innerHTML={data().tulisan}></div>
+					<div 
+						className="focus:outline-none p-2 bg-green-200 overflow-auto rounded" 
+						contenteditable 
+						onInput={x => setData({tulisan: x.target.innerHTML})} 
+						innerHTML={tulisan()}
+						// innerHTML={data().tulisan}
+					></div>
 					<textarea name="" value={data().tulisan} onInput={x => setData({tulisan: x.target.value})} id="" cols="30" rows="10" className="bg-green-200 p-2 focus:outline-none rounded"></textarea>
 				</div>
 			</div>
